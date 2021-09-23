95b430c33d8aad65db933c91707df2d979cd6195
Yanfu <yanfu-guo@sjtu.edu.cn>
2021-09-22 23:42:05 -0400

    finish lab1 front-end, need test

--- lab1/kotlinChatter/app/src/main/java/edu/umich/yanfuguo/kotlinChatter/ChattStore.kt
+++ lab1/kotlinChatter/app/src/main/java/edu/umich/yanfuguo/kotlinChatter/ChattStore.kt
@@ -66,27 +66,37 @@ object ChattStore {
     }
 
     fun getChatts(context: Context, completion: () -> Unit) {
-        val getRequest = JsonObjectRequest(serverUrl+"getchatts/",
-            { response ->
-                chatts.clear()
-                val chattsReceived = try { response.getJSONArray("chatts") } catch (e: JSONException) { JSONArray() }
-                for (i in 0 until chattsReceived.length()) {
-                    val chattEntry = chattsReceived[i] as JSONArray
-                    if (chattEntry.length() == nFields) {
-                        chatts.add(Chatt(username = chattEntry[0].toString(),
-                            message = chattEntry[1].toString(),
-                            timestamp = chattEntry[2].toString()))
-                    } else {
-                        Log.e("getChatts", "Received unexpected number of fields: " + chattEntry.length().toString() + " instead of " + nFields.toString())
-                    }
-                }
-                completion()
-            }, { completion() }
-        )
+        val request = Request.Builder()
+            .url(serverUrl+"getimages/")
+            .build()
 
-        if (!this::queue.isInitialized) {
-            queue = newRequestQueue(context)
-        }
-        queue.add(getRequest)
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("getChatts", "Failed GET request")
+                completion()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val chattsReceived = try { JSONObject(response.body?.string() ?: "").getJSONArray("chatts") } catch (e: JSONException) { JSONArray() }
+
+                    chatts.clear()
+                    for (i in 0 until chattsReceived.length()) {
+                        val chattEntry = chattsReceived[i] as JSONArray
+                        if (chattEntry.length() == nFields) {
+                            chatts.add(Chatt(username = chattEntry[0].toString(),
+                                message = chattEntry[1].toString(),
+                                timestamp = chattEntry[2].toString(),
+                                imageUrl = chattEntry[3].toString(),
+                                videoUrl = chattEntry[4].toString(),
+                            ))
+                        } else {
+                            Log.e("getChatts", "Received unexpected number of fields " + chattEntry.length().toString() + " instead of " + nFields.toString())
+                        }
+                    }
+                    completion()
+                }
+            }
+        })
     }
 }
