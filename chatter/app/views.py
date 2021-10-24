27b6b92cc56a1ca5cc7e89fc940872c33d8caf7b
Ubuntu <ubuntu@ip-172-31-3-237.us-east-2.compute.internal>
2021-10-24 00:25:21 +0000

    lab3 back end

--- chatter/app/views.py
+++ chatter/app/views.py
@@ -75,6 +75,30 @@ def postchatt(request):
                    '(%s, %s);', (username, message))
     return JsonResponse({})
 
+@csrf_exempt
+def getaudio(request):
+        if request.method != 'GET':
+                return HttpResponse(status=404)
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM audio ORDER BY time DESC;')
+        rows = cursor.fetchall()
+
+        response = {}
+        response['chatts'] = rows
+        return JsonResponse(response)
+
+
+@csrf_exempt
+def postaudio(request):
+    if request.method != 'POST':
+        return HttpResponse(status=404)
+    json_data = json.loads(request.body)
+    username = json_data['username']
+    message = json_data['message']
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO chatts (username, message) VALUES '
+                   '(%s, %s);', (username, message))
+    return JsonResponse({})
 
 @csrf_exempt
 def postmaps(request):
